bfc2154bd460f14c723c79901d977f891ae21252
ykaurarshpreet <ykaur.arshpreet@gmail.com>
2025-10-16 20:57:39 -0700

    third commit

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const ipAddress = document.getElementById('ip-address');
 const locationDiv = document.getElementById('location');
-const timezone = document.getElementById('timezone');
+const timezoneDiv = document.getElementById('timezone');
 const isp = document.getElementById('isp');
+const mapDiv = document.getElementById('map');
+const searchBtn = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
 
 
 
@@ -23,20 +26,33 @@ document.addEventListener('DOMContentLoaded', async function(){
         //updating ipaddress div with the data we got back from the api
         ipAddress.textContent = `IP Address ${data.ip}`
         locationDiv.textContent = `Location ${data.location.city} ${data.location.region} ${data.location.postalCode}`
-        timezone.textContent = `Timezone UTC- ${data.location.timezone}`
+        timezoneDiv.textContent = `Timezone UTC- ${data.location.timezoneDiv}`
         isp.textContent = `ISP- ${data.isp}`
         console.log(data);
+
+    const map = L.map(mapDiv).setView(
+      [data.location.lat, data.location.lng],
+      13,
+    );
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
 
     } catch(error) {
         console.error(error.message);
     }
+
 });
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+// searchBtn.addEventListener('click', async function(){
+//     try{
+//         const response = await fetch ('https://geo.ipify.org/api/v2/country,city?apiKey=at_eXFIkij3E8EOA8KMaghXnuXDiTaL3&ipAddress=8.8.8.8'); 
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+//     } catch(error) {
+//         console.error(error.message);
+//     }
+// });
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
